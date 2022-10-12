0e59a9c2fb1776d87d479b9557952ec7a9080789
Ilya Milinteev <ilyamilint@gmail.com>
2022-10-12 22:39:59 +0300

    comit 7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectIsRefreshing } from 'redux/selectors';
 import { refreshUser } from 'redux/operationsAuth';
-import { Layout } from './Layout';
+import { SiteNavigate } from './SiteNavigate/SiteNavigate';
 import { RestrictedRoute } from './RestrictedRoute/RestrictedRoute';
 import { PrivateRoute } from './PrivateRoute/PrivateRoute';
 
@@ -32,37 +32,37 @@ export const App = () => {
     'Refretch from Data'
   ) : (
     <div>
+      <SiteNavigate />
+
       <Routes>
-        <Route path="/goit-react-hw-08-phonebook" element={<Layout />}>
-          <Route path="/home" element={<HomePage />} />
-          <Route
-            path="/signUpPage"
-            element={
-              <RestrictedRoute
-                redirectTo="/contactsPage"
-                component={<SignUpPage />}
-              />
-            }
-          />
-          <Route
-            path="/logInPage"
-            element={
-              <RestrictedRoute
-                redirectTo="/contactsPage"
-                component={<LogInPage />}
-              />
-            }
-          />
-          <Route
-            path="/contactsPage"
-            element={
-              <PrivateRoute
-                redirectTo="/logInPage"
-                component={<ContactsPage />}
-              />
-            }
-          />
-        </Route>
+        <Route path="/home" element={<HomePage />} />
+        <Route
+          path="/signUpPage"
+          element={
+            <RestrictedRoute
+              redirectTo="/contactsPage"
+              component={<SignUpPage />}
+            />
+          }
+        />
+        <Route
+          path="/logInPage"
+          element={
+            <RestrictedRoute
+              redirectTo="/contactsPage"
+              component={<LogInPage />}
+            />
+          }
+        />
+        <Route
+          path="/contactsPage"
+          element={
+            <PrivateRoute
+              redirectTo="/logInPage"
+              component={<ContactsPage />}
+            />
+          }
+        />
         {/* <Route path="/goit-react-hw-08-phonebook" element={<Blah />} /> */}
       </Routes>
 
