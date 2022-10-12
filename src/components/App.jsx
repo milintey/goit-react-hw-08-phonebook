30081ad3b33cc9e64c70a76ad2c83a3e56924972
Ilya Milinteev <ilyamilint@gmail.com>
2022-10-12 19:52:40 +0300

    comit 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,14 @@ import { Layout } from './Layout';
 import { RestrictedRoute } from './RestrictedRoute/RestrictedRoute';
 import { PrivateRoute } from './PrivateRoute/PrivateRoute';
 
+function Blah() {
+  return (
+    <div>
+      <h2>This is the blah page</h2>
+    </div>
+  );
+}
+
 export const App = () => {
   const dispatch = useDispatch();
   const isRefreshing = useSelector(selectIsRefreshing);
@@ -55,6 +63,7 @@ export const App = () => {
             }
           />
         </Route>
+        <Route path="/goit-react-hw-08-phonebook" element={<Blah />} />
       </Routes>
 
       <GlobalStyle />
